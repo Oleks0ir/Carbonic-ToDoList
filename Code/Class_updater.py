012c1cc5a8afede19f8a6e992d67083db9d7b516
Alex0ir-Admin-User <alexandr.kolbeck@gmail.com>
2024-02-07 21:43:24 +0100

    The class has been updated with separate step discription

--- Code/Class_updater.py
+++ Code/Class_updater.py
@@ -1,16 +1,29 @@
+
+#---Import Area---------------------
 import datetime
 
+#---Test Area-----------------------
+#Testing function for checking the connection between Library and Main code
 def Hello_World():
 	print("Hello World")
 
 
+#---Class Area-----------------------
+
+# Class 'Item' is created to store information of tasks. It stores: Title, Date of creation,
+# Discription of the file and its importance level
+
 class Item:
+	
 	def __init__(self, title, discription, importance):
 		self.title = title
-		self.date = datetime.datetime.now()
 		self.discription = discription
 		self.importance = importance
-	
+
+#		Date is being stored separatly, without users involved
+		self.date = datetime.datetime.now()
+		
+#	The output function
 	def __str__(self):
 		return f"{self.title}({self.importance}) created on {self.date.strftime('%x  %X')}: \n {self.discription} "
 
--- Code/Main.py
+++ Code/Main.py
@@ -3,6 +3,6 @@ import Class_updater as C_upd
 
 #C_upid.Hello_World()
 
-Item1 = C_upid.Item("Test Title", "This is test title which is used to display the discription of the file", 3)
+Item1 = C_upd.Item("Test Title", "This is test title which is used to display the discription of the file", 3)
 
 print(Item1)
