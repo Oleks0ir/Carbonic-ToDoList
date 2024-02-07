da37a8d53355df99d2f96fd08241142fdecdfa1a
Alex0ir-Admin-User <alexandr.kolbeck@gmail.com>
2024-02-07 21:26:41 +0100

    Created and implemented the class 'Item'. Linked to main program.

--- Code/Main.py
+++ Code/Main.py
@@ -1,3 +1,8 @@
 import Module_1 as M1
 
+
 M1.Hello_World()
+
+Item1 = M1.Item("Test Title", "This is test title which is used to display the discription of the file", 3)
+
+print(Item1)
--- Code/Module_1.py
+++ Code/Module_1.py
@@ -1,2 +1,19 @@
+import datetime
+
 def Hello_World():
 	print("Hello World")
+
+
+class Item:
+	def __init__(self, title, discription, importance):
+		self.title = title
+		self.date = datetime.datetime.now()
+		self.discription = discription
+		self.importance = importance
+	
+	def __str__(self):
+		return f"{self.title}({self.importance}) created on {self.date.strftime('%x  %X')}: \n {self.discription} "
+
+
+
+
