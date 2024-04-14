49156baa33a76f5b341006c376fe396fe9c0b7dd
Alex0ir-Admin-User <alexandr.kolbeck@gmail.com>
2024-04-14 16:54:45 +0200

    Added a Tester to the pipeline

--- Code/Main.py
+++ Code/Main.py
@@ -1,8 +1,8 @@
-import Class_updater as C_upd
+import Class_updater as Clupdate
 
 
 #C_upid.Hello_World()
 
-Item1 = C_upd.Item("Test Title", "This is test title which is used to display the discription of the file", 3)
+Item1 = Clupdate.Item("Test Title", "This is test title which is used to display the discription of the file", 3)
 
 print(Item1)
