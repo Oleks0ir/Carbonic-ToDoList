09fe3a162c234c8af43fcb2cff6aeab2b539075b
Alex0ir-Admin-User <alexandr.kolbeck@gmail.com>
2024-04-22 21:13:16 +0200

    Minor clearences

--- Code/Main.py
+++ Code/Main.py
@@ -4,7 +4,3 @@ import Class_updater as Cupdate
 def test():
 	return True
 ############################
-
-Item1 = Cupdate.Item("Test Title", "This is test title which is used to display the discription of the file", 3)
-
-print(Item1)
